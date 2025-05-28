9e4b27c9e77b43049aaeb055b8fd6c583a6cd48c
Hilmar <hilmar@dolphinics.com>
2025-05-28 11:19:01 +0200

    .

--- ucx_custom_app_progs/my_uct_testing/test_uct.c
+++ ucx_custom_app_progs/my_uct_testing/test_uct.c
@@ -1,5 +1,12 @@
 #include <uct/api/uct.h>
 #include <assert.h>
+#include <stdlib.h>
+
+#define ERROR_CHECK_UCS_OK(func_name, error) \
+if (error != UCS_OK) {  \
+    fprintf(stderr, "Error: %s failed, error code %d", func_name, (int)error); \
+    exit(EXIT_FAILURE); \
+}
 
 int main(int argc, char **argv)
 {
@@ -8,11 +15,15 @@ int main(int argc, char **argv)
 
     /* Initialize context */
     ucs_status_t status = ucs_async_context_create(UCS_ASYNC_MODE_THREAD_SPINLOCK, &async);
-    assert(UCS_OK == status);
+    ERROR_CHECK_UCS_OK("ucs_async_context_create", status)
 
     /* Create a worker object */
     status = uct_worker_create(async, UCS_THREAD_MODE_SINGLE, &worker);
-    assert(UCS_OK == status);
+    ERROR_CHECK_UCS_OK("ucs_async_context_create", status)
+
+    // /* Search for the desired transport */
+    // status = dev_tl_lookup(&cmd_args, &if_info);
+    // assert(status == UCS_OK);
 
     /* Cleanup */
     uct_worker_destroy(worker);
