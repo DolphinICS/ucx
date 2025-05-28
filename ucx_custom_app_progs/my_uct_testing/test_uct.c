67733a983ea2ffed9e3ff1e461e5d00aad53ff31
Hilmar <hilmar@dolphinics.com>
2025-05-28 15:52:45 +0200

    .

--- ucx_custom_app_progs/my_uct_testing/test_uct.c
+++ ucx_custom_app_progs/my_uct_testing/test_uct.c
@@ -17,14 +17,31 @@ static ucs_status_t dev_tl_lookup()
     status = uct_query_components(&components, &num_components);
     ERROR_CHECK_UCS_OK("uct_query_components", status)
 
-    // for (int cmpt_index = 0; cmpt_index < num_components; ++cmpt_index) {
+    printf("num_components = %u\n", num_components);
 
-    //     uct_component_attr_t   component_attr;
+    for (int cmpt_index = 0; cmpt_index < num_components; cmpt_index++) {
 
-    //     component_attr.field_mask = UCT_COMPONENT_ATTR_FIELD_MD_RESOURCE_COUNT;
-    //     status = uct_component_query(components[cmpt_index], &component_attr);
-    //     ERROR_CHECK_UCS_OK("uct_query_components", status)
-    // }
+        printf("cmpt_index == %d\n", cmpt_index);
+
+        uct_component_attr_t component_attr;
+
+        component_attr.field_mask = UCT_COMPONENT_ATTR_FIELD_MD_RESOURCE_COUNT;
+        status = uct_component_query(components[cmpt_index], &component_attr);
+        ERROR_CHECK_UCS_OK("uct_query_components", status)
+
+        printf("number of resources %u\n", component_attr.md_resource_count);
+
+        component_attr.field_mask = UCT_COMPONENT_ATTR_FIELD_MD_RESOURCES;
+        component_attr.md_resources = alloca(sizeof(*component_attr.md_resources) *
+                                             component_attr.md_resource_count);
+        status = uct_component_query(components[cmpt_index], &component_attr);
+        ERROR_CHECK_UCS_OK("uct_query_components", status)
+
+        
+        
+    }
+
+    return UCS_OK;
 }
 
 int main(int argc, char **argv)
@@ -40,9 +57,9 @@ int main(int argc, char **argv)
     status = uct_worker_create(async, UCS_THREAD_MODE_SINGLE, &worker);
     ERROR_CHECK_UCS_OK("uct_worker_create", status)
 
-    // /* Search for the desired transport */
-    // status = dev_tl_lookup(&cmd_args, &if_info);
-    // assert(status == UCS_OK);
+    /* Search for the desired transport */
+    status = dev_tl_lookup();
+    ERROR_CHECK_UCS_OK("dev_tl_lookup", status)
 
     /* Cleanup */
     uct_worker_destroy(worker);
