dfc5d8db0eeae4dda1155c21148cddbd2f650da6
Hilmar <hilmar@dolphinics.com>
2025-05-28 12:30:59 +0200

    .

--- ucx_custom_app_progs/my_uct_testing/test_uct.c
+++ ucx_custom_app_progs/my_uct_testing/test_uct.c
@@ -4,8 +4,27 @@
 
 #define ERROR_CHECK_UCS_OK(func_name, error) \
 if (error != UCS_OK) {  \
-    fprintf(stderr, "Error: %s failed, error code %d", func_name, (int)error); \
+    fprintf(stderr, "Error %s, line %u: %s failed, error code %d", __FUNCTION__, __LINE__, func_name, (int)error); \
     exit(EXIT_FAILURE); \
+}
+
+static ucs_status_t dev_tl_lookup()
+{
+    uct_component_h *components;
+    unsigned num_components;
+    ucs_status_t status;
+
+    status = uct_query_components(&components, &num_components);
+    ERROR_CHECK_UCS_OK("uct_query_components", status)
+
+    // for (int cmpt_index = 0; cmpt_index < num_components; ++cmpt_index) {
+
+    //     uct_component_attr_t   component_attr;
+
+    //     component_attr.field_mask = UCT_COMPONENT_ATTR_FIELD_MD_RESOURCE_COUNT;
+    //     status = uct_component_query(components[cmpt_index], &component_attr);
+    //     ERROR_CHECK_UCS_OK("uct_query_components", status)
+    // }
 }
 
 int main(int argc, char **argv)
@@ -19,7 +38,7 @@ int main(int argc, char **argv)
 
     /* Create a worker object */
     status = uct_worker_create(async, UCS_THREAD_MODE_SINGLE, &worker);
-    ERROR_CHECK_UCS_OK("ucs_async_context_create", status)
+    ERROR_CHECK_UCS_OK("uct_worker_create", status)
 
     // /* Search for the desired transport */
     // status = dev_tl_lookup(&cmd_args, &if_info);
